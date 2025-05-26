01eec6c7f0055cebd9e2fa9a375d7876ce945d30
Nerkasov Dmitry <nekrasovdwitry@gmail.com>
2025-05-26 14:01:13 +0300

    Added back-button and refactoring notes

---
+++ src/features/notes/ui/notes-layout/notes-layout.tsx
@@ -0,0 +1,37 @@
+import { type ReactNode } from "react";
+import styles from "./styles.module.scss";
+import { type ViewMode } from "../view-mode/view-mode";
+
+interface NotesLayoutProps {
+  viewMode: ViewMode;
+  notes: ReactNode[];
+  hasNextPage: boolean;
+  isFetchingNextPage: boolean;
+  isPending: boolean;
+  cursorRef: (node: HTMLDivElement | null) => void;
+}
+
+export function NotesLayout({
+  viewMode,
+  notes,
+  hasNextPage,
+  isFetchingNextPage,
+  isPending,
+  cursorRef,
+}: NotesLayoutProps) {
+  return (
+    <>
+      <div className={viewMode === "list" ? styles.notes : styles.notesGrid}>
+        {notes}
+      </div>
+
+      {hasNextPage && (
+        <div ref={cursorRef} className={styles.observer}>
+          {isFetchingNextPage && "Loading more..."}
+        </div>
+      )}
+
+      {isPending && <div className={styles.loading}>Loading notes...</div>}
+    </>
+  );
+} 
