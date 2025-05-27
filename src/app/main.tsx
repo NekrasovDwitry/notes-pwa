fe95b789286de7fea4a22ae2610be640688b9b5e
Nerkasov Dmitry <nekrasovdwitry@gmail.com>
2025-05-27 18:15:11 +0300

    Added basic PWA support

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,6 +5,19 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { enableMocking } from "@/shared/api/mocks";
 
+// Регистрация сервис-воркера
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/pwa/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful');
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
+
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
